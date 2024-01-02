13a3a407a2deafef55046820d9b6ae8c5954d093
AfricaLived <ismakrem101@gmail.com>
2024-01-02 02:15:25 -0500

    chat page layout adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
     }
   }
   return (
-    <div className="w-screen min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-sky-500 via-orange-200 to-yellow-600">
+    <div className="w-[100%] min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-sky-500 via-orange-200 to-yellow-600">
       <div className="absolute right-0 m-3 rounded-full drop-shadow-xl items-center">
         <UserButton afterSignOutUrl='/' />
       </div>
